8789f384afa32e7da4a95d358fb244a20c02fc20
Revilo321 <58446652+Revilo321@users.noreply.github.com>
2022-06-01 11:04:07 +0200

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,72 @@ import Head from 'next/head';
 
 const Index = () => {
   return (
-    <div>
+    <div className="p-32">
       <Head>
         <title>Intern assignment</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p className="text-xl font-sans text-gray-800 mx-10 my-10 ">Website</p>
+      <h1 className="text-center text-dp-headline-3-t font-alternative font-semibold">
+        Kære praktikansøger,
+      </h1>
+      <p className="text-xl font-sans text-gray-800 my-10 ">
+        Scopet for denne opgave er at finde ud af hvor meget praktikanten ved om
+        de teknologier som Dreamplan bruger. Dette projekt er en standard
+        React/Tailwind/NextJs/Typescript template som minder om Dreamplans
+        nuværende frontend projekt. Du har fire timer til opgaven, og den
+        afleveres ved at create a fork med en beskrivelse af de features du har
+        udviklet.
+      </p>
+      <h1 className="text-dp-headline-2-m font-alternative font-semibold">
+        Intro
+      </h1>
+      <p className="text-xl font-sans text-gray-800 mt-4 mb-10 ">
+        Npm install projektet og opret en branch som inkluderer dit navn. Når du
+        har fået installeret projektet, skal du gå ind på figma, her vil du
+        finde det design som du skal udvikle ud fra. Til dagligt bliver alt
+        designet til Dreamplans udviklere i figma.
+        <br />
+        <br />
+        Figma link:
+        https://www.figma.com/file/gKl0SZkthgwqR8434FL5fL/Intern-Assignment---Dat?node-id=0%3A1
+        <br />
+        <br />
+        Bestræb dig på at udvikle så det ligner designet på figma så meget som
+        muligt.
+        <br />
+        <br />
+        Du skal udvikle så det er responsive i mobil, tablet og desktop view med
+        tailwindCSS. Et godt råd er at udvikle mobile first og at starte med at
+        kigge i tailwind.config filen, hvor vi allerede har sat pre-defineret
+        styles ind som Dreamplan bruger.
+      </p>
+      <h1 className="text-dp-headline-4-m font-alternative font-semibold">
+        1. Udvikle Q and A og Overblik
+      </h1>
+      <p className="text-xl font-sans text-gray-800 mt-4 mb-10 ">
+        Du skal udvikle de to sider Q and A og Overblik du finder på figma
+        linket i introen. Start med at oprette to pages under src/pages og prøv
+        dig frem med at udvikle siderne gerne med komponenter, som skal
+        genbruges for at holde din kode dry.
+      </p>
+      <h1 className="text-dp-headline-4-m font-alternative font-semibold">
+        2. Header
+      </h1>
+      <p className="text-xl font-sans text-gray-800 mt-4 mb-10 ">
+        Du skal udvikle en header som er gennemgående på begge sider. Det skal
+        være muligt at navigere til den anden side fra headeren.
+      </p>
+      <h1 className="text-dp-headline-4-m font-alternative font-semibold">
+        3. Footer(Ekstra)
+      </h1>
+      <p className="text-xl font-sans text-gray-800 mt-4 mb-10 ">
+        Du skal udvikle en footer som er gennemgående på begge sider. Det skal
+        være muligt at trykke på social media ikonerne og blive henvist til
+        Dreamplans SoMe.
+      </p>
+      <h1 className="text-xl font-sans text-gray-800 mt-4 mb-10 font-semibold'">
+        Vær gerne kreativ og gør dit bedste.
+      </h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       lg: '1024px',
       xl: '1280px',
       '2xl': '1440px',
+      '3xl': '1920px',
     },
     fontFamily: {
       sans: ['Montserrat', 'Roboto', 'sans-serif'],
@@ -16,6 +17,8 @@ module.exports = {
       colors: {
         dp: {
           'blue-500': '#0700F6',
+          'light-blue-400': '#99E2E7',
+          'light-blue-800': '#33585A',
           'grayscale-100': '#FFFFFF',
           'grayscale-300': '#D5D9E3',
           'grayscale-800': '#2F3032',
@@ -23,6 +26,11 @@ module.exports = {
         },
       },
       fontSize: {
+        'dp-headline-2-t': '48px',
+        'dp-headline-3-t': '36px',
+        'dp-headline-2-m': '30px',
+        'dp-headline-4-m': '22px',
+        'dp-headline-5-m': '20px',
         'dp-headline-6-m': '18px',
         'dp-bodytext-l-m': '16px',
         'dp-bodytext-m-dt': '14px',
