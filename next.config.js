360d27bc46197c2318f9e59b28434daf5b892016
Lars Buur <larsbuur@gmail.com>
2022-12-19 15:45:10 +0100

    cleanup code and update

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Index = () => {
   return (
-    <div className=" p-48 text-center text-dp-headline-2-t font-alternative font-bold">Q&A</div>
+    <div className=" p-48 text-center text-dp-headline-2-t font-alternative font-bold">
+      Q&A
+    </div>
   );
 };
 
